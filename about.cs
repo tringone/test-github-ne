3c8994906a4203e1220612a282ea8f745095e3a8
Tri Ngo <143936102+tringone@users.noreply.github.com>
2024-05-23 21:55:55 +0700

    Update about.cs

--- about.cs
+++ about.cs
@@ -3,6 +3,6 @@ namespace HelloWorld
         {
         static void Main(string[] args)
             {
-            System.Console.WriteLine(“Hello World!”);
+            System.Console.WriteLine(“Hello World nha Tri Ngo”);
             }
         }
